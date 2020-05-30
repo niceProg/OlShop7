6c8c1c4fcd0be393da30bb1a2d55163271edd70b
Wisnu Yumna <wisnuyumna@Wisnus-MacBook-Air.local>
2020-05-30 15:44:08 +0700

    study laravel 7

---
+++ resources/views/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container text-center">
+    <h2>Products</h2>
+
+    <div class="row">
+
+        @foreach ($allProducts as $product)
+            <div class="col-3">
+                <img class="card-img-top" src="{{ asset('default-product.jpg') }}" alt="Card image cap">
+                <div class="card-body">
+                    <h4 class="card-title">{{ $product->name }}</h4>
+                    <p class="card-text">{{ $product->description }}</p>
+                </div>
+                <div class="card-body">
+                    <a href="#" class="card-link">Add to cart</a>
+                </div>
+            </div>
+        @endforeach
+
+    </div>
+</div>
+@endsection
